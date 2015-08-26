3c259a6946e80febfd42db01f4bec6c68df4dafc
Kevin McNally <kevinmcnally26@gmail.com>
2015-08-26 19:08:29 +0100

    Update foreman.sh

--- foreman.sh
+++ foreman.sh
@@ -3,3 +3,5 @@ Configure temp foreman to build our infra structure
 Pulls in the puppet forge module.
 
 Then we use hammer scripts to build hosts.
+
+The config in here will be dumped in db and moved to the real foreman this hosts builds.
