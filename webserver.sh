2f5225932039717bc63063e5bee71a031db77e19
Kevin McNally <kevinmcnally26@gmail.com>
2015-08-26 19:00:19 +0100

    Update webserver.sh

--- webserver.sh
+++ webserver.sh
@@ -1,1 +1,8 @@
-Configure temp webserver to build our infra structure
+echo INSTALL PACKAGES
+yum install dhcp syslinux tftp-server xinetd httpd vim -y
+
+echo HTTP MEDIA SETUP - MOUNTED ONLY
+mkdir -p ${DROOT}/{${KSDIR},${PACKDIR}}
+mount -o loop /dev/cdrom  ${DROOT}/${PACKDIR} || echo Looks like cdrom is already mounted
+systemctl enable httpd.service
+systemctl restart httpd.service
