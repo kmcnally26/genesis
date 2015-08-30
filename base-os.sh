947926cd468b54f67e339fc7069a2ecf78f217a1
Kevin McNally <kevinmcnally26@gmail.com>
2015-08-30 14:55:39 +0100

    Update base-os.sh

--- base-os.sh
+++ base-os.sh
@@ -1,17 +1,19 @@
+# This is settings for when genesis is plugged into DC and not for building with inet connection
+
 source /root/server-genesis/genesis.properties
 
-yum install -y $PACKAGES
+yum install -y $PACKAGES  # see genesis.properties
 
 cat << EOF > /etc/hosts
 127.0.0.1   localhost localhost.localdomain localhost4 localhost4.localdomain4
 ::1         localhost localhost.localdomain localhost6 localhost6.localdomain6
-$IPADDRESS $SERVERNAME ${SERVERNAME%%.*}
+$GENESIS_IP $GENESIS_SERVER ${GENESIS_SERVER%%.*}
 
 EOF
 
 cat << EOF > /etc/hostname
 NETWORKING=yes
-HOSTNAME=$SERVERNAME
+HOSTNAME=$GENESIS_SERVER
 
 EOF
 
@@ -20,9 +22,8 @@ DEVICE=eth0
 TYPE=Ethernet
 ONBOOT=yes
 BOOTPROTO=static
-IPADDR=192.168.136.100
-GATEWAY=192.168.136.2
-NETMASK=255.255.255.0
+IPADDR=$GENESIS_IP
+NETMASK=$GENESIS_NETMASK
 
 EOF
 
