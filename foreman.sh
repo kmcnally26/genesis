385b5d8cc1112439d0fa24c29950420b47e4b6f6
Kevin McNally <kevinmcnally26@gmail.com>
2015-08-30 14:45:29 +0100

    Delete foreman.sh

--- foreman.sh
+++
@@ -1,7 +0,0 @@
-Configure temp foreman to build our infra structure
-
-Pulls in the puppet forge module.
-
-Then we use hammer scripts to build hosts.
-
-The config in here will be dumped in db and moved to the real foreman this hosts builds.
